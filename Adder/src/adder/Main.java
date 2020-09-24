e9b9228502886d8ba032db525fa8f4719abf63a6
Zackary Contreras <zackaryc007@gmail.com>
2020-09-24 15:33:43 -0500

    Adjusted addArguments
    adjusted function to allow for first input to be "-" and then add arguments as negatives

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,22 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println(e);
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int res = 0;
+
+        if (args[0].contains("-")) {
+            for (int i = 1; i < args.length; i++) {
+                res -= Integer.valueOf(args[i]);
+            }
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                res += Integer.valueOf(args[i]);
+            }
+        }
+        return res;
     }
 }
